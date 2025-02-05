b1f158e78c1e36cdc252633413a053878c5afbae
bishaMoh <bshar8755@gmial.com>
2025-02-05 21:16:28 +0200

    interducing the UI interface

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanScore = 0;
+let computerScore = 0;
 const choices = ["rock", "paper", "scissors"];
 
 
@@ -6,63 +8,58 @@ function getComputerChoice(){
     const random = choices[Math.floor(Math.random() * choices.length)];
     return random;
 }
+let computerSelection = getComputerChoice();
 
 
-// get the human choice
-function getHumanChoice(){
-    let human = prompt("what is your choice: ");
-    let humanChoice = human.toLowerCase();
-    for(i = 0; i < 3; i++){
-        if (humanChoice === choices[i]){
-            return humanChoice;
-        }
-    }
-        return ("please inter a vaild choice!");
-    
-}
 
 // palying a single round
+
 function playRound(a, b){
+    let result = "";
+
     if (a === b){
-        return "it is a draw";
+        result = `it is a tie! both chose ${a}`;
+
     }else if (
         (a === "rock" && b === "paper") || (a === "scissors" && b === "rock") ||
         (a === "paper" && b === "scissors")
     ){
        computerScore++;
-       console.log("you lost this round!");
-       return computerScore; 
+ 
+        result = "you lost!";
+
+        
     }else{
         humanScore++;
-        console.log("you won this round!");
-        return humanScore;
+
+         result = "you won!";
+    }
+        document.querySelector("#result").textContent = result;
+        document.querySelector("#result-text").textContent = `User: ${humanScore} - CPU: ${computerScore}`;
+        
+        if (humanScore === 5 || computerScore === 5) {
+           announceWinner();
     }
 }
 
-
-
-// playing intire game of 5 rounds
-function playGame() {
-    humanScore = 0;
-    computerScore = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        console.log(`\nRound ${i + 1}`);
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    console.log("\nFinal Results:");
-    if (humanScore > computerScore) {
-        console.log("Congratulations! You won the game!");
-    } else if (humanScore < computerScore) {
-        console.log("Sorry, you lost the game!");
+function announceWinner() {
+    let winnerText = '';
+    if (humanScore === 5) {
+        winnerText = 'Congratulations! You won the game!';
     } else {
-        console.log("It's a tie!");
+        winnerText = 'Sorry, the computer won the game.';
     }
+
+    // Update the winner text
+    document.getElementById('winner-text').textContent = winnerText;
+
+    document.querySelector('#btn1').disabled = true;
+    document.querySelector('#btn2').disabled = true;
+    document.querySelector('#btn3').disabled = true;
 }
 
-// Start the game
-playGame();
+document.querySelector('#btn1').addEventListener('click', () => playRound("rock", computerSelection));
+document.querySelector('#btn2').addEventListener('click', () => playRound("paper", computerSelection));
+document.querySelector('#btn3').addEventListener('click', () => playRound("scissors", computerSelection));
+
 
